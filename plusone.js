6d32eb43d557ef78fca1c40b52532402c5c38964
paridhishah56 <paridhishah56@gmail.com>
2025-03-06 19:24:53 +0530

    plusone

---
+++ plusone.js
@@ -0,0 +1,18 @@
+
+//ex 1:
+// let digit = [1,2,3];
+//  let a = digit[digit.length -1 ] + 1;
+//  digit[digit.length - 1] = a;
+
+//  console.log(digit);
+
+//ex 2:
+   
+let digit = [4,3,2,1];
+
+let a = digit[digit.length - 1] + 1;
+digit[digit.length - 1 ] = a
+
+console.log(digit);
+
+  
